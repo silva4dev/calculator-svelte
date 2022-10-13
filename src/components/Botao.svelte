bc4134b955a36f25a2e22ac0b61ecf7a2b0e52e7
Lucas Alves da Silva <web4dev.lucas@outlook.com>
2022-10-13 04:46:40 -0300

    inicial

---
+++ src/components/Botao.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  export let texto: string;
+  export let duplo: boolean = false;
+  export let triplo: boolean = false;
+  export let operacao: boolean = false;
+  export let destaque: boolean = false;
+  export let onClick: (valor: string) => void = () => {};
+</script>
+
+<button
+  class="botao"
+  on:click={() => onClick(texto)}
+  class:triplo
+  class:duplo
+  class:operacao
+  class:destaque
+>
+  {texto}
+</button>
+
+<style>
+  .botao {
+    margin: 0;
+    border: 1px solid var(--cor-fundo);
+    flex-basis: 25%;
+  }
+
+  .duplo {
+    flex-basis: 50%;
+  }
+
+  .triplo {
+    flex-basis: 75%;
+  }
+
+  .operacao {
+    background-color: orange;
+    color: white;
+  }
+
+  .destaque {
+    background-color: red;
+    color: white;
+  }
+</style>
---
+++ src/components/Calculadora.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+  
+  import CalculadoraModel from "../models/CalculadoraModel";
+
+  import Botao from "./Botao.svelte";
+  import Linha from "./Linha.svelte";
+  import Tela from "./Tela.svelte";
+
+  let calc = new CalculadoraModel();
+
+  const numeroDigitado = (num: string) => calc = calc.numeroDigitado(num);
+  const operacaoDigitada = (op: string) => calc = calc.operacaoDigitada(op);
+  const pontoDigitado = () => calc = calc.pontoDigitado();
+  const calcular = () => calc = calc.calcular();
+  const limpar = () => calc = calc.limpar();
+
+</script>
+
+<div class="calculadora">
+  <Tela valor={calc.valor} />
+  <Linha>
+    <Botao texto="AC" onClick={limpar} triplo destaque />
+    <Botao texto="/"  onClick={operacaoDigitada} triplo />
+  </Linha>
+
+  <Linha>
+    <Botao texto="7" onClick={numeroDigitado} />
+    <Botao texto="8" onClick={numeroDigitado} />
+    <Botao texto="9" onClick={numeroDigitado} />
+    <Botao texto="*" onClick={operacaoDigitada} operacao />
+  </Linha>
+
+  <Linha>
+    <Botao texto="4" onClick={numeroDigitado} />
+    <Botao texto="5" onClick={numeroDigitado} />
+    <Botao texto="6" onClick={numeroDigitado} />
+    <Botao texto="+" onClick={operacaoDigitada} operacao />
+  </Linha>
+
+  <Linha>
+    <Botao texto="1" onClick={numeroDigitado} />
+    <Botao texto="2" onClick={numeroDigitado} />
+    <Botao texto="3" onClick={numeroDigitado} />
+    <Botao texto="-" onClick={operacaoDigitada} operacao />
+  </Linha>
+
+  <Linha>
+    <Botao texto="0" onClick={numeroDigitado} duplo />
+    <Botao texto="," onClick={pontoDigitado} />
+    <Botao texto="=" onClick={calcular} destaque />
+  </Linha>
+</div>
+
+<style>
+  .calculadora {
+    background-color: var(--cor-fundo);
+    height: 220px;
+    width: 150px;
+    padding: 10px;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
